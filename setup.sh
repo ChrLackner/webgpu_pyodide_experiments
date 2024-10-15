2988d27a8f1cfa0d944ab40f09cfdf6c5e633b68
Matthias Hochsteger <mhochsteger@cerbsim.com>
2024-10-15 12:05:39 +0200

    cleanup, readme

--- setup.sh
+++
@@ -1,8 +0,0 @@
-rm -f *.tar.bz2
-wget https://ngsolve.org/files/ngsolve_pyodide_0.26.2.tar.bz2
-tar xvf ngsolve_pyodide_0.26.2.tar.bz2
-cd pyodide
-ln -s ../index.html
-ln -s ./pyodide.js
-ln -s ../webgpu.py
-ln -s ../shader.wgsl
